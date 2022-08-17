7ccdb37b17a7edd42e4e17bbb6cd23633bd7dab7
coder <1157180781@qq.com>
2022-08-17 15:12:53 +0800

    ✨ feat(unoCss):新增unoCss依赖和配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import path from 'path'
-
+import Unocss from "unocss/vite";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { presetUno, transformerDirectives } from 'unocss'
+import presetRemToPx from '@unocss/preset-rem-to-px'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: './',
-  plugins: [react()],
-    resolve: {
-        alias: {
-          '@': path.resolve(__dirname, './src'),
-        },
-      },
+  base: './',
+  plugins: [react(), Unocss({
+    transformers: [transformerDirectives()],
+      presets: [
+                presetUno(),
+                presetRemToPx()
+            ],
+  })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
