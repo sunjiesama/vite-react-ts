d1502f003165d6c80ac617726780f50b5cdba2b7
coder <1157180781@qq.com>
2022-08-17 15:04:00 +0800

    📃 docs:add readme file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-function App() {
-  return (
-    <div >
+import React from "react"
+
+const App: React.FC = () => {
+  return  <div>
      Click on the Vite and React logos to learn more
     </div>
-  )
 }
 
 export default App
